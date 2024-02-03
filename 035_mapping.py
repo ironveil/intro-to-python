5307a04162c5cde3e7e235437a299710b3c9fdd6
IronVeil <git@ironveil.cloud>
2024-02-03 14:46:07 +0000

    Completed 035

--- 035_mapping.py
+++ 035_mapping.py
@@ -35,6 +35,12 @@ print("Function: add_one_hundred_to_numbers")
 
 # Return a new list of each number with 100 added
 def add_one_hundred_to_numbers(numbers):
+  new_list = []
+
+  for num in numbers:
+    new_list.append(num + 100)
+
+  return new_list
   pass
 
 check_that_these_are_equal(
