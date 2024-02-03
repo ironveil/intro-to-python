6f4d8c9716390e2d320356486434c1b5a835cd21
IronVeil <git@ironveil.cloud>
2024-02-03 14:54:35 +0000

    Completed 038

--- 038_dict_operations.py
+++ 038_dict_operations.py
@@ -67,7 +67,18 @@ print("Function: count_words_by_length")
 # Since there are two words of length 3, etc.
 
 def count_words_by_length(words):
-  pass
+  length_dict = {}
+  
+  for word in words:
+    length = len(word)
+
+    if length not in length_dict:
+      length_dict[length] = 1
+    
+    else:
+      length_dict[length] += 1
+
+  return length_dict
 
 check_that_these_are_equal(
   count_words_by_length(["hat", "cat", "I", "bird"]),
